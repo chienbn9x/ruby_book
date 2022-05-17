e67b4f79f7b9251d9c39e1df5cfc0232f37aff44
chiennv <chienbn9x@gmail.com>
2022-05-17 20:03:58 +0700

    validate model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ApplicationRecord
+
+  validates :Url, :Title, :Publication_year, :Authors, :Description, :Main_image, :Price, presence: true
+  validates :Discount, :Final_price, :Amount, :Stop_business, presence: true
+  validates :Url, uniqueness: true
+
   belongs_to :category
+  has_many :orderitems, dependent: :delete_all
+  has_many :reviews, dependent: :delete_all
 end
--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,2 +1,6 @@
 class Promotion < ApplicationRecord
+
+    validates :Title, :Code, :Discount_percent, :Discount_price, :Discount_type, presence: true
+    validates :Min_order_value, :Max_order_value, :Start_date, :End_date, :Number_of_use, presence: true
+
 end
