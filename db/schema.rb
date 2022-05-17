be4ece93de0bd389f0263aaef1931ac87c126d3f
chiennv <chienbn9x@gmail.com>
2022-05-17 12:59:17 +0700

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,114 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_17_045540) do
+ActiveRecord::Schema.define(version: 2022_05_16_185829) do
 
+  create_table "addresses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "Contact_receiver"
+    t.string "Contact_phone"
+    t.text "Contact_address"
+    t.boolean "Addr_default"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orderitems", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.bigint "product_id", null: false
+    t.integer "Quantity"
+    t.boolean "Reviewed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_orderitems_on_order_id"
+    t.index ["product_id"], name: "index_orderitems_on_product_id"
+  end
+
+  create_table "orders", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "Contact_receiver"
+    t.string "Contact_phone"
+    t.text "Contact_address"
+    t.float "Sub_total"
+    t.float "Discount"
+    t.float "Total"
+    t.integer "Status"
+    t.text "Note"
+    t.datetime "Order_date"
+    t.datetime "Delivery_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orderstatuses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.integer "Status"
+    t.datetime "Update_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_orderstatuses_on_order_id"
+  end
+
+  create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.string "Url"
+    t.text "Title"
+    t.string "Publication_year"
+    t.text "Authors"
+    t.text "Description"
+    t.string "Main_image"
+    t.float "Price"
+    t.float "Discount"
+    t.float "Final_price"
+    t.integer "Amount"
+    t.boolean "Stop_business"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "promotions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "Title"
+    t.string "Code"
+    t.float "Discount_percent"
+    t.float "Discount_price"
+    t.integer "Discount_type"
+    t.float "Min_order_value"
+    t.float "Max_order_value"
+    t.date "Start_date"
+    t.date "End_date"
+    t.integer "Number_of_use"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.string "Title"
+    t.text "Content"
+    t.integer "Rating"
+    t.integer "Like_count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_reviews_on_product_id"
+  end
+
+  create_table "sub_reviews", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "review_id", null: false
+    t.text "Content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["review_id"], name: "index_sub_reviews_on_review_id"
+  end
+
+  add_foreign_key "orderitems", "orders"
+  add_foreign_key "orderitems", "products"
+  add_foreign_key "orderstatuses", "orders"
+  add_foreign_key "products", "categories"
+  add_foreign_key "reviews", "products"
+  add_foreign_key "sub_reviews", "reviews"
 end
